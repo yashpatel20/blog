959a986f13429cd71fd78b089101588e744318d8
Yashpatel20 <yash.work10@gmail.com>
2020-03-26 18:35:58 +0530

    added a testing route for resetting the db

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
 app.use("/api/users", userRouter);
 app.use("/api/blogs", blogRouter);
 app.use("/api/login", loginRouter);
